291fc6fb935c3a8a58771f8d8731138663e9b047
uma <blog3170@gmail.com>
2018-05-05 13:32:59 +0900

    add setEnv

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::fs::{File};
 use std::path::Path;
 use std::env;
+use std::process::Command;
 
 fn main() {
     let root;
@@ -21,7 +22,6 @@ fn main() {
         None => panic!("Impossible to get your home dir!"),
     }
 
-    println!("{:?}", &root);
     match fs::read_dir(&root) {
         Ok(dir) => {
             let mut ignore_exists = false;
@@ -85,7 +85,31 @@ fn main() {
         }
     }
     
-
+    setEnv(&bin_path);
  
     println!("finish");
+}
+
+fn setEnv(bin_path: &Path) {
+    let mut arg = "setx /M path \"%path%;".to_string();
+    arg.push_str(bin_path.to_str().unwrap());
+    arg.push_str("\"");
+    println!("{:?}", arg);
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .arg("/C")
+                .arg(arg)
+                .output()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg("$env:COMPLUS_MDA")
+                .env("HELLO", "hello, world")
+                .output()
+                .expect("failed to execute process")
+    };
+
+    let hello = output.stdout;
+    println!("{}", std::str::from_utf8(&hello).unwrap());
 }
